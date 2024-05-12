e62a9b2d085833032cdbec66c97835b3cc6d906f
gjohnhazel <john@johnhazel.com>
2024-05-12 13:01:13 -0500

    update astro config to my url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-theme-cody.netlify.app',
+  site: 'https://nerds.johnhazel.com',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
